1c2fcc70f70a6ebdf7124a3385c9a86aadad1c69
firminoneto11 <firminoneto11@hotmail.com>
2025-03-25 10:07:13 -0300

    test: adds test cases

--- tests/src/app/services/test_core.py
+++ tests/src/app/services/test_core.py
@@ -1,8 +1,10 @@
 from typing import TYPE_CHECKING
 
 from pytest import mark
+from sqlalchemy import func, select
 
 from src.app.services.core import CoreService
+from src.domain.models import Pokemon
 from src.infra.repositories.pokemons import PokemonRepo
 
 if TYPE_CHECKING:
@@ -12,8 +14,9 @@ if TYPE_CHECKING:
 
 
 class TestCoreService:
-    def setup_class(self):
-        print("Setting up")
+    async def count_pokemons(self, session: "AsyncSession"):
+        stmt = select(func.count()).select_from(Pokemon)
+        return (await session.scalars(stmt)).one()
 
     @mark.parametrize(argnames="pokemon", argvalues=["pikachu", "charizard"])
     async def test_get_pokemon_details(
@@ -26,9 +29,14 @@ class TestCoreService:
             pokemon_repo=PokemonRepo(session=session), data_fetcher=data_fetcher_mock
         )
 
+        amount_before = await self.count_pokemons(session=session)
         result = await svc.get_pokemon_details(name=pokemon)
+        amount_after = await self.count_pokemons(session=session)
 
+        assert not amount_before
+        assert amount_after == 1
+        assert isinstance(result["id"], str)
         assert result["name"] == pokemon
-
-    def teardown_class(self):
-        print("Tearing down")
+        assert isinstance(result["base_experience"], int)
+        assert isinstance(result["abilities"], list)
+        assert isinstance(result["weight"], int)
